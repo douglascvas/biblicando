d606ac74d381f05b81ad40e1e328f5a8c551e9ed
Douglas Carvalho Vasconcelos <douglascvas@gmail.com>
2016-05-29 22:09:25 +0200

    Added submodules.

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var proxy = require('http-proxy-middleware');
+
+var app = express();
+
+app.use('/web', proxy({target: 'http://localhost:3005', changeOrigin: true}));
+app.use('/api', proxy({target: 'http://localhost:3005', changeOrigin: true}));
+app.listen(3000);
