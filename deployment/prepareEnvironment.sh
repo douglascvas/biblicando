e26d1e96f824b821a13ccf75b0ee8fd9c1448206
douglascvas <douglascvas@gmail.com>
2016-10-31 21:22:03 +0100

    Added nodejs to deployment script

--- deployment/prepareEnvironment.sh
+++ deployment/prepareEnvironment.sh
@@ -6,28 +6,36 @@ echo "local    - configure the environment in the local host\n"
 echo "vagrant  - configure the environment in the vagrant machine (under IP 192.168.33.10)\n"
 echo "server   - configure the environment in the remote biblicando server machine\n\n"
 read -p ">> " INVENTORY
-read -p "Install jenkins? [Y/n]" INSTALL_JENKINS
-read -p "Install docker? [Y/n]" INSTALL_DOCKER
+read -p "Install jenkins? [y/N]" INSTALL_JENKINS
+read -p "Install docker? [y/N]" INSTALL_DOCKER
+read -p "Install nodejs? [y/N]" INSTALL_NODEJS
 
-if [ $INSTALL_JENKINS  -ne 'y' ] && [ $INSTALL_JENKINS  -ne 'Y' ];
+if [ "$INSTALL_JENKINS"  == "y" ] || [ "$INSTALL_JENKINS"  == "Y" ];
 then
-  INSTALL_JENKINS = 0
+  INSTALL_JENKINS=1
 else
-  INSTALL_JENKINS = 1;
+  INSTALL_JENKINS=0
 fi
 
-if [ $INSTALL_DOCKER  -ne 'y' ] && [ $INSTALL_DOCKER  -ne 'Y' ];
+if [ "$INSTALL_DOCKER"  == "y" ] || [ "$INSTALL_DOCKER"  == "Y" ];
 then
-  INSTALL_DOCKER = 0
+  INSTALL_DOCKER=1
 else
-  INSTALL_DOCKER = 1;
+  INSTALL_DOCKER=0
+fi
+
+if [ "$INSTALL_NODEJS"  == "y" ] || [ "$INSTALL_NODEJS"  == "Y" ];
+then
+  INSTALL_NODEJS=1
+else
+  INSTALL_NODEJS=0
 fi
 
 if [ -z $INVENTORY ]
 then
   echo "Invalid option. Aborting..."
 else
-  ansible-playbook -i inventories/$INVENTORY/hosts site.yml --extra-vars "install_jenkins=$INSTALL_JENKINS install_docker=$INSTALL_DOCKER"
+  ansible-playbook -i inventories/$INVENTORY/hosts site.yml --extra-vars "install_jenkins=$INSTALL_JENKINS install_docker=$INSTALL_DOCKER install_nodejs=$INSTALL_NODEJS"
 fi
 
 
