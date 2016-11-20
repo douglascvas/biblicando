68ba9bfe9c8dcb8abf64e5bb8e6f4564c9504afe
douglascvas <douglascvas@gmail.com>
2016-11-20 22:12:35 +0100

    Adding gulp task for tests.

--- index.js
+++ index.js
@@ -5,26 +5,3 @@ var app = express();
 app.use('/api', proxy({target: 'http://localhost:3005', changeOrigin: true}));
 app.use('/', proxy({target: 'http://localhost:3000', changeOrigin: true}));
 app.listen(3010);
-
-// const sourceMapSupport = require('source-map-support');
-// sourceMapSupport.install();
-//
-// const Backend = require("./backend/build/main/server").Server;
-// const Frontend = require("./frontend/server");
-//
-// var backend = Backend.build();
-//
-// backend.initialize()
-//   .then(()=> {
-//     return backend.start();
-//   })
-//   .then(app=> {
-//     console.log(app.use);
-//     const frontend = Frontend.build(app);
-//     frontend.initialize();
-//     return frontend.start();
-//   })
-//   .catch(e => {
-//     backend.logger.error(e.stack);
-//   });
-//
